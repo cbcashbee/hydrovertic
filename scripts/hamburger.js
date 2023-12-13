ecd5af06f5e820609a0c0dd8b4a58e729b8c423e
cbcashbee <sillebobjerg@hotmail.com>
2023-12-13 10:27:01 +0100

    Hamburger org

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,13 +1,21 @@
 // Funktion til at skifte menuens synlighed
 function toggleMenu() {
   console.log("Funktionen toggleMenu blev kaldt");
-  var menu = document.querySelector('.menu');
-  menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('show-menu');
+  const menuList = document.querySelectorAll('.menu li');
+  menuList.forEach(item => {
+    if (menu.classList.contains('show-menu')) {
+      item.style.display = 'block'; // Viser listen når menuen er vist
+    } else {
+      item.style.display = 'none'; // Skjuler listen når menuen ikke er vist
+    }
+  });
+  console.log("Menuen blev vist/gemt gennem funktionen toggleMenu");
 }
 
-
-  // Et array af menu punkter og tilhørende URL'er
-  var menuItems = [
+// Et array af menu punkter og tilhørende URL'er
+const menuItems = [
   { text: "Dashboard", url: "dashboard.html" },
   { text: "Datalogging", url: "datalogging.html" },
   { text: "Indstillinger", url: "indstillinger.html" },
@@ -15,12 +23,14 @@ function toggleMenu() {
 ];
 
 // Loop gennem array for at tilføje punkter til menuen
-var menuList = document.querySelector('.menu');
-for (var i = 0; i < menuItems.length; i++) {
-  var menuItem = document.createElement('li');
-  var link = document.createElement('a');
+const menuList = document.querySelector('.menu');
+for (let i = 0; i < menuItems.length; i++) {
+  const menuItem = document.createElement('li');
+  const link = document.createElement('a');
   link.href = menuItems[i].url; // Indsæt url fra objektet
   link.textContent = menuItems[i].text;
+  console.log("Link text:", menuItems[i].text);
   menuItem.appendChild(link);
   menuList.appendChild(menuItem);
+  menuItem.style.display = 'none'; // Starter med at skjule alle menu punkter
 }
