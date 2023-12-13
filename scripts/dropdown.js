a8ef29fbaa42cfb55b228e0dc7cad6ef668c0255
cbcashbee <sillebobjerg@hotmail.com>
2023-12-13 19:29:38 +0100

    Mobil done + validated

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,27 +1,27 @@
 // dropdown.js
 
-function myFunction() {
-    var dropdown1 = document.getElementById("myDropdown1");
+function kulturFunction() {
+  const dropdown3 = document.getElementById("kulturDropdown");
+  dropdown3.classList.toggle("show");
+}
+
+function stativFunction() {
+  const dropdown2 = document.getElementById("stativDropdown");
+  dropdown2.classList.toggle("show");
+}
+
+function vaerdierFunction() {
+    const dropdown1 = document.getElementById("vaerdierDropdown");
     dropdown1.classList.toggle("show");
-  }
+}
   
-  function myOtherFunction() {
-    var dropdown2 = document.getElementById("myDropdown2");
-    dropdown2.classList.toggle("show");
-  }
-  
-  function myThirdFunction() {
-    var dropdown3 = document.getElementById("myDropdown3");
-    dropdown3.classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (event) {
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
       for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+        const openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
